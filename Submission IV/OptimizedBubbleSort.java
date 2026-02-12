dde33f9fc44362161192eabd09435b24d7b7f0b3
madhubhashana11 <madhubhashana2326@gmail.com>
2026-02-13 03:29:58 +0530

    OptimizedBubbleSort.java
    Implemented an optimized version of bubble sort that reduces unnecessary comparisons.

---
+++ Submission IV/OptimizedBubbleSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class OptimizedBubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n-1; i++) {
+            swapped = false;
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+        bubbleSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
